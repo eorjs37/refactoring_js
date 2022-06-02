73b674aa2244335d61b1153f9de83b11b622c49f
maxgunchoi <chleorjs37@gmail.com>
2022-06-03 08:41:32 +0900

    checkhand 진행중

---
+++ src/check-hand.js
@@ -0,0 +1,118 @@
+
+checkStraightFlush = function(){
+    return false;
+}
+
+checkFullHouse = function(){
+    return false;
+}
+
+checkFlush = function(){
+    return false;
+}
+
+checkStraight = function(){
+    return false;
+}
+
+checkTwoPair = function(){
+    return false;
+}
+
+
+checkFourOfKind = function(){
+    return false;
+}
+
+checkThreeOfKind = function(){
+    return false;
+}
+
+checkPair = function(){
+    return false;
+}
+
+var getValues = function(hand){
+    console.log(hand);
+    var values = [];
+    for(var  i =0 ; i < hand.length ; i++){
+        console.log(hand[i]);
+        values.push(hand[i][0]);
+    }
+
+    console.log(values);
+    return values;
+}
+
+var countDuplicates = function(values){
+    console.log('values are : '+ values);
+    var numberOfDuplicates = 0;
+    var duplicatesOfThisCard;
+
+    for(var  i = 0 ;i < values.length ; i++){
+        duplicatesOfThisCard = 0;
+        console.log(numberOfDuplicates);
+        console.log(duplicatesOfThisCard);
+
+        if(values[i] === values[0]){
+            duplicatesOfThisCard+=1;
+        }
+        if(values[i] === values[1]){
+            duplicatesOfThisCard+=1;
+        }
+        if(values[i] === values[2]){
+            duplicatesOfThisCard+=1;
+        }
+        if(values[i] === values[3]){
+            duplicatesOfThisCard+=1;
+        }
+        if(values[i] === values[4]){
+            duplicatesOfThisCard+=1;
+        }
+        if(duplicatesOfThisCard > numberOfDuplicates){
+            numberOfDuplicates = duplicatesOfThisCard
+        }
+
+    }
+
+    return numberOfDuplicates;
+}
+
+var checkHand = function(hand){
+    var values = getValues(hand);
+    var number = countDuplicates(values);
+
+    if(checkStraightFlush(hand)){
+        return 'straight flush';
+    }
+    else if(number === 4){
+        return 'four of a kind';
+    }
+    else if(checkFullHouse(hand)){
+        return 'full house';
+    }
+    else if(checkFlush(hand)){
+        return 'flush';
+    }
+    else if(checkStraight(hand)){
+        return 'straight';
+    }
+    else if(number === 3){
+        return 'three of a kind';
+    }
+    else if(checkTwoPair(hand)){
+        return 'two pair';
+    }
+    else if(checkPair(hand)){
+        return 'pair';
+    }
+    else if(number === 2){
+        return 'pair';
+    }
+    else{
+        return 'high card'
+    }
+}
+//debugger;
+console.log('Value of checkHand is ' + checkHand(['2-H','3-C','4-D','5-H','2-C']));
+console.log('Value of checkHand is ' + checkHand(['3-H','3-C','3-D','5-H','2-H']));
