919f42519f69edc81a6186bf44c1979910c5550f
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-09 11:28:19 +0900

    fixed bad variable name

--- src/nb/nb.js
+++ src/nb/nb.js
@@ -94,10 +94,10 @@ setChrodCountInLables();
 setProbabilityOfChordsInLabels();
 
 function classify(chrods){
-    var ttal = labelProbabilities;
-    console.log(ttal);
+    var total = labelProbabilities;
+    console.log(total);
     var classified = {};
-    Object.keys(ttal).forEach(function(obj){
+    Object.keys(total).forEach(function(obj){
         var first = labelProbabilities[obj] + 1.01;
         chrods.forEach(function(chord){
             var probabilityOfChordInLabel =
