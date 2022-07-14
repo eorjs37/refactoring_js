da7441cfa3f9cd37f2c65a3135ef47e71267a310
maxgunchoi <chleorjs37@gmail.com>
2022-07-15 08:42:37 +0900

    커밋

---
+++ src/nb/nb-review.js
@@ -0,0 +1,126 @@
+fs = require("fs");
+//노래 관련코드들
+imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
+someWhereOverTheRainbow = ["c", "em", "f", "g", "am"];
+tooManyCooks = ["c", "g", "f"];
+iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
+babyOneMoreTime = ["cm", "g", "bb", "eb", "fm", "ab"];
+creep = ["g", "gsus4", "b", "bsus4", "c", "cmsus4", "cm6"];
+army = ["ab", "ebm7", "dbadd9", "fm7", "bbm", "abmaj7", "ebm"];
+paperBag = [
+  "bm7",
+  "e",
+  "c",
+  "g",
+  "b7",
+  "f",
+  "em",
+  "a",
+  "cmaj7",
+  "em7",
+  "a7",
+  "f7",
+  "b",
+];
+
+toxic = ["cm", "eb", "g", "cdim", "eb7", "d", "db7", "ab", "gmaj7", "g7"];
+bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
+blankSong = [];
+
+var songs = [];
+var labels = [];
+var allChords = [];
+var labelCounts = [];
+var labelProbabilities = [];
+var chordCountsInLabels = {};
+var probabilityOfChordsInLabels = {};
+
+function train(chords, label) {
+  songs.push([label, chords]);
+  labels.push(label);
+  for (var i = 0; i < chords.length; i++) {
+    if (!allChords.includes(chords[i])) {
+      allChords.push(chords[i]);
+    }
+  }
+
+  if (!!Object.keys(labelCounts).includes(label)) {
+    labelCounts[label] = labelCounts[label] + 1;
+  } else {
+    labelCounts[label] = 1;
+  }
+}
+
+function getNumberOfSongs() {
+  return songs.length;
+}
+function setLabelProbabilities() {
+  Object.keys(labelCounts).forEach(function (label) {
+    var numberOfSongs = getNumberOfSongs();
+    labelProbabilities[label] = labelCounts[label] / numberOfSongs;
+  });
+}
+function setChrodCountsInLabels() {
+  songs.forEach(function (i) {
+    if (chordCountsInLabels[i[0]] === undefined) {
+      chordCountsInLabels[i[0]] = {};
+    }
+
+    i[1].forEach(function (j) {
+      if (chordCountsInLabels[i[0]][j] > 0) {
+        chordCountsInLabels[i[0]][j] = chordCountsInLabels[i[0]][j] + 1;
+      } else {
+        chordCountsInLabels[i[0]][j] = 1;
+      }
+    });
+  });
+}
+function setProbabilityOfChordsInLabels() {
+  probabilityOfChordsInLabels = chordCountsInLabels;
+  Object.keys(probabilityOfChordsInLabels).forEach(function (i) {
+    Object.keys(probabilityOfChordsInLabels[i]).forEach(function (j) {
+      probabilityOfChordsInLabels[i][j] =
+        (probabilityOfChordsInLabels[i][j] * 1.0) / songs.length;
+    });
+  });
+}
+
+train(imagine, "easy");
+train(someWhereOverTheRainbow, "easy");
+train(tooManyCooks, "easy");
+
+train(iWillFollowYouIntoTheDark, "medium");
+train(babyOneMoreTime, "medium");
+train(creep, "medium");
+
+train(paperBag, "hard");
+train(toxic, "hard");
+train(bulletproof, "hard");
+
+setLabelProbabilities();
+setChrodCountsInLabels();
+setProbabilityOfChordsInLabels();
+
+function classify(chords) {
+  var total = labelProbabilities;
+  console.log(total);
+
+  var classififed = {};
+  Object.keys(total).forEach(function (obj) {
+    var first = labelProbabilities[obj] + 1.01;
+    chords.forEach(function (chord) {
+      var probabilityOfChordInLabel = probabilityOfChordsInLabels[obj][chord];
+      if (probabilityOfChordInLabel === undefined) {
+        first + 1.01;
+      } else {
+        first = first * (probabilityOfChordInLabel + 1.01);
+      }
+    });
+
+    classififed[obj] = first;
+  });
+  console.log(classififed);
+}
+
+classify(["d", "g", "e", "dm"]);
+classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
