fdab7a2762ea0da8436623f95037a90580304761
maxgunchoi <chleorjs37@gmail.com>
2022-08-04 08:12:21 +0900

    instanceof 추가

--- src/crud/countWord.js
+++ src/crud/countWord.js
@@ -1,22 +1,23 @@
-class EnglishWord {
+class Word {
   constructor(word) {
     this.word = word;
   }
-
   count() {
     return this.word.length;
   }
-}
 
-class JapaenseWord {
-  constructor(word) {
-    this.word = word;
-  }
-
-  count() {
-    return this.word.length;
+  lookUp() {
+    if (this instanceof JapaenseWord) {
+      return `일본어주소/${this.word}`;
+    } else {
+      return `영어주소/${this.word}`;
+    }
   }
 }
+
+class EnglishWord extends Word {}
+
+class JapaenseWord extends Word {}
 
 const japanseWord = new JapaenseWord("히라가나");
 const englishWord = new EnglishWord("ABC");
