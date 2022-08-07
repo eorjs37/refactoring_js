bf87dbbebc1c4907a5cbf1e8e7b1929362a8297e
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-07 15:12:26 +0900

    8장 완료

--- src/extends/wolf-goat.js
+++ src/extends/wolf-goat.js
@@ -1,33 +1,37 @@
-class Agent {
-  constructor(name, type) {
-    this.name = "name";
-    if (Math.random() > 0.5) {
-      this.type = "user";
-    } else {
-      this.type = "project";
-    }
-  }
-
-  static makeProjectOrUser(agent) {
-    if (agent.type === "user") {
-      return Object.assign(Object.create(new User()), agent);
-    } else {
-      return Object.assign(Object.create(new Project()), agent);
-    }
-  }
+function coinToss() {
+  return Math.random() > 0.5;
 }
-
-class User extends Agent {
+class User {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
   sayName() {
     return `my name is ${this.name}`;
   }
 }
 
-class Project extends Agent {
+class Project {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
   sayTheName() {
     return `the project name is ${this.name}`;
   }
 }
 
-const agent = new Agent("name");
-const projectOrUser = Agent.makeProjectOrUser(agent);
+let agent;
+if (coinToss()) {
+  agent = new User("name", "user");
+} else {
+  agent = new Project("name", "project");
+}
+
+const wish = require("wish");
+
+if (agent.type === "user") {
+  wish(agent.sayName() === "my name is name");
+} else {
+  wish(agent.sayTheName() === "the project name is name");
+}
