37cbcbedba1381ccea25f9a73e06193f67f006b9
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-08 20:32:24 +0900

    9장 진행중

---
+++ src/oop/person-10.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(whatIs) {
+    this.whatIs = whatIs;
+  }
+  log(number) {
+    console.log(this.whatIs(number));
+  }
+  static binaryAware(number) {
+    return Number("0b" + number);
+  }
+  static binaryOblivious(number) {
+    return number;
+  }
+}
+
+const personOne = new Person(Person.binaryAware);
+const personTwo = new Person(Person.binaryOblivious);
+
+[personOne, personTwo].forEach((person) => {
+  person.log(10);
+});
