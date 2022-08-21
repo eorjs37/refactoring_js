becbe102973305dda3bdfc1544ee1b080f391e37
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-21 18:20:36 +0900

    함수 추출 완료

--- src/nb/nb.js
+++ src/nb/nb.js
@@ -1,29 +1,35 @@
 fs = require("fs");
 
-var easy = "easy";
-var medium = "medium";
-var hard = "hard";
+function setup() {
+  console.log("setup");
+  songs = [];
+  allChords = new Set();
+  labelCounts = new Map();
+  labelProbabilities = new Map();
+  chordCountsInLabels = new Map();
+  probabilityOfChordsInLabels = new Map();
+}
 
-//노래 관련 코드들
-imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
-someWhereOverTheRainbow = ["c", "em", "f", "g", "am"];
-tooManyCooks = ["c", "g", "f"];
+function setDifficulties() {
+  easy = "easy";
+  medium = "medium";
+  hard = "hard";
+}
 
-iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
-babyOneMoreTime = ["cm", "g", "bb", "eb", "eb", "fm", "ab"];
-creep = ["g", "gsus4", "b", "bsus4", "c", "cmsus4", "cm6"];
+function setSongs() {
+  //노래 관련 코드들
+  imagine = ["c", "cmaj7", "f", "am", "dm", "g", "e7"];
+  someWhereOverTheRainbow = ["c", "em", "f", "g", "am"];
+  tooManyCooks = ["c", "g", "f"];
 
-paperBag = ["bm7", "e", "c", "g", "b7", "f", "em", "A", "cmaj7", "em7", "a7", "f7", "b"];
-toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
-bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
+  iWillFollowYouIntoTheDark = ["f", "dm", "bb", "c", "a", "bbm"];
+  babyOneMoreTime = ["cm", "g", "bb", "eb", "eb", "fm", "ab"];
+  creep = ["g", "gsus4", "b", "bsus4", "c", "cmsus4", "cm6"];
 
-var songs = [];
-var allChords = new Set();
-var labelCounts = new Map();
-var labelProbabilities = new Map();
-var chordCountsInLabels = new Map();
-var probabilityOfChordsInLabels = new Map();
-
+  paperBag = ["bm7", "e", "c", "g", "b7", "f", "em", "a", "cmaj7", "em7", "a7", "f7", "b"];
+  toxic = ["cm", "eb", "g", "cdim", "eb7", "d7", "db7", "ab", "gmaj7", "g7"];
+  bulletproof = ["d#m", "g#", "b", "f#", "g#m", "c#"];
+}
 function train(chords, label) {
   songs.push({
     label,
@@ -74,6 +80,9 @@ function setProbabilityOfChordsInLabels() {
 }
 
 function trainAll() {
+  setDifficulties();
+  setup();
+  setSongs();
   train(imagine, easy);
   train(someWhereOverTheRainbow, easy);
   train(tooManyCooks, easy);
@@ -97,7 +106,6 @@ function setLabelsAndProbabilites() {
 
 function classify(chords) {
   var smoothing = 1.01;
-  console.log(labelProbabilities);
   var classified = new Map();
   labelProbabilities.forEach(function (_probabilites, difficutly) {
     var first = labelProbabilities.get(difficutly) + smoothing;
@@ -112,12 +120,10 @@ function classify(chords) {
     classified.set(difficutly, first);
   });
 
-  console.log(classified);
-
   return classified;
 }
 
-classify(["d", "g", "e", "em"]);
+//classify(["d", "g", "e", "em"]);
 
 var wish = require("wish");
 
@@ -127,7 +133,14 @@ describe("the file", () => {
     var classifed = classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
     wish(classifed.get("easy") === 1.3433333333333333);
     wish(classifed.get("medium") === 1.5060259259259259);
-    wish(classifed.get("hard") === 1.5060259259259259);
+    wish(classifed.get("hard") === 1.6884223991769547);
+  });
+
+  it("clssifies again", () => {
+    var classifed = classify(["d", "g", "e", "em"]);
+    wish(classifed.get("easy") === 2.023094827160494);
+    wish(classifed.get("medium") === 1.6552851851851849);
+    wish(classifed.get("hard") === 2.080511600763603);
   });
 
   it("label probabilites", () => {
