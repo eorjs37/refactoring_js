b686bf14b1b35a509978de7249b0d9cd8df0fd60
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-31 08:06:29 +0900

    countWord  생성

--- src/crud/countWord.js
+++ src/crud/countWord.js
@@ -1,58 +1,27 @@
-class Word {
-  constructor(word, language) {
+class EnglishWord {
+  constructor(word) {
     this.word = word;
-    this.language = language;
   }
+
   count() {
     return this.word.length;
   }
 }
 
-class EnglishWord extends Word {
+class KoreaWord {
   constructor(word) {
-    super(word, "English");
+    this.word = word;
   }
 
-  lookUp() {
-    return `영어주소/${this.word}`;
+  count() {
+    return this.word.length;
   }
 }
 
-class JapaenseWord extends Word {
-  constructor(word) {
-    super(word, "Japanese");
-  }
+const koreaWord = new KoreaWord("개");
+const englishWord = new EnglishWord("dog");
 
-  lookUp() {
-    return `일본어주소/${this.word}`;
-  }
-}
-
-const japanseWord = new JapaenseWord("히라가나");
-const englishWord = new EnglishWord("ABC");
-
-const wish = require("wish");
-const deepEqual = require("deep-equal");
-
-//인터페이스 테스트
-wish(japanseWord.word === "히라가나");
-wish(japanseWord.lookUp() === "일본어주소/히라가나");
-wish(japanseWord.count() === 4);
-
-wish(englishWord.word === "ABC");
-wish(englishWord.lookUp() === "영어주소/ABC");
-wish(englishWord.count() === 3);
-
-//내부 테스트
-wish(typeof japanseWord === "object");
-wish(typeof JapaenseWord === "function");
-wish(japanseWord instanceof JapaenseWord);
-wish(japanseWord instanceof Word);
-wish(!(JapaenseWord instanceof Word));
-
-wish(japanseWord.constructor === JapaenseWord);
-console.log(Object.getPrototypeOf(JapaenseWord));
-wish(Object.getPrototypeOf(JapaenseWord) === Word);
-
-//약간 개략적인 테스트
-wish(deepEqual(Object.getPrototypeOf(japanseWord), {}));
+console.log(koreaWord.count());
+console.log(koreaWord.word);
+console.log(englishWord.count());
+console.log(englishWord.word);
