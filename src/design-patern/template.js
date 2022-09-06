40030f591702f14a5904008135937ae53eb57155
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-07 08:08:12 +0900

    조건부를 Person으로 이동

--- src/design-patern/template.js
+++ src/design-patern/template.js
@@ -2,8 +2,8 @@
  템플릿 메서드 : 최소한의 변형으로 같은 목적을 수행하는 두 알고리즘이 있을때 유용
 */
 class Person {
-  constructor(binaryKnower) {
-    this.binaryKnower = binaryKnower;
+  constructor(typeOfPerson) {
+    this.typeOfPerson = typeOfPerson;
   }
   whatIs(number) {
     return number;
@@ -11,15 +11,18 @@ class Person {
   whatIsBinary(number) {
     return Number("0b" + number);
   }
+  log(number) {
+    if (this.typeOfPerson === "binary knower") {
+      console.log(this.whatIsBinary(10));
+    } else {
+      console.log(this.whatIs(10));
+    }
+  }
 }
 
-const personOne = new Person(true); //이진법을 이해하는 사람
-const personTwo = new Person(false); //이진법을 이해못하는 사람
+const personOne = new Person("binary knower"); //이진법을 이해하는 사람
+const personTwo = new Person("binary oblivious"); //이진법을 이해못하는 사람
 
 [personOne, personTwo].forEach((person) => {
-  if (person.binaryKnower) {
-    console.log(person.whatIsBinary(10));
-  } else {
-    console.log(person.whatIs(10));
-  }
+  person.log(10);
 });
