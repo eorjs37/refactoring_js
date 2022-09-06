ad6a7c70d1a342c5da7e6e1e2ce5da1c996ed0f0
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-07 07:55:54 +0900

    8장 완료

--- src/agent/Agent.js
+++ src/agent/Agent.js
@@ -2,53 +2,37 @@ function coinToss() {
   return Math.random() > 0.5;
 }
 
-class Agent {
+class User {
   constructor(name, type) {
     this.name = name;
     this.type = type;
-
-    // if (coinToss()) {
-    //   this.type = "user";
-    // } else {
-    //   this.type = "project";
-    // }
   }
-
-  //   static makeProjectUser(agent) {
-  //     if (agent.type === "user") {
-  //       return Object.assign(Object.create(new User()), agent);
-  //     } else {
-  //       return Object.assign(Object.create(new Project()), agent);
-  //     }
-  //   }
-}
-
-let agent;
-
-if (coinToss()) {
-  agent = new Agent();
-} else {
-}
-
-class User extends Agent {
   sayName() {
     return `my name is ${this.name}`;
   }
 }
 
-class Project extends Agent {
+class Project {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
   sayTheName() {
     return `the project name is ${this.name}`;
   }
 }
 
-// const agent =
+let agent;
 
-// const projectUser = Agent.makeProjectUser(agent);
+if (coinToss()) {
+  agent = new User("name", "user");
+} else {
+  agent = new Project("name", "project");
+}
 
-// const wish = require("wish");
-// if (projectUser.type === "user") {
-//   wish(projectUser.sayName() === "my name is name");
-// } else {
-//   wish(projectUser.sayTheName() === "the project name is name");
-// }
+const wish = require("wish");
+if (agent.type === "user") {
+  wish(agent.sayName() === "my name is name");
+} else {
+  wish(agent.sayTheName() === "the project name is name");
+}
