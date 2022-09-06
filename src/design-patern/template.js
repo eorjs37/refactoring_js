5c050dbb3fe4095c4e2e3ffd22eb6bcd715a719f
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-07 08:16:03 +0900

    template 메서드 완료

--- src/design-patern/template.js
+++ src/design-patern/template.js
@@ -1,27 +1,27 @@
 /*
  템플릿 메서드 : 최소한의 변형으로 같은 목적을 수행하는 두 알고리즘이 있을때 유용
+ 공통메서드 부분을 상위 클래스로 옮기는것.
 */
 class Person {
-  constructor(typeOfPerson) {
-    this.typeOfPerson = typeOfPerson;
-  }
-  whatIs(number) {
-    return number;
-  }
-  whatIsBinary(number) {
-    return Number("0b" + number);
-  }
   log(number) {
-    if (this.typeOfPerson === "binary knower") {
-      console.log(this.whatIsBinary(10));
-    } else {
-      console.log(this.whatIs(10));
-    }
+    console.log(this.whatIs(number));
   }
 }
 
-const personOne = new Person("binary knower"); //이진법을 이해하는 사람
-const personTwo = new Person("binary oblivious"); //이진법을 이해못하는 사람
+class BinaryKnower extends Person {
+  whatIs(number) {
+    return Number("0b" + number);
+  }
+}
+
+class BinaryOblivious extends Person {
+  whatIs(number) {
+    return number;
+  }
+}
+
+const personOne = new BinaryKnower(); //이진법을 이해하는 사람
+const personTwo = new BinaryOblivious(); //이진법을 이해못하는 사람
 
 [personOne, personTwo].forEach((person) => {
   person.log(10);
