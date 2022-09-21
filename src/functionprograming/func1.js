84288cbb32e7ffc506290a9f12c34e583d364956
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-22 08:28:54 +0900

    11.2 완료

---
+++ src/functionprograming/func1.js
@@ -0,0 +1,8 @@
+const arr1 = [2, 3, 4];
+
+arr1.forEach((ele) => console.log(ele));
+
+const arr2 = arr1.map((ele) => ele * 2);
+
+console.log(arr1);
+console.log(arr2);
---
+++ src/functionprograming/func2.js
@@ -0,0 +1,9 @@
+const x = [1, 2, 3, 4];
+
+x.splice(1);
+
+console.log(x);
+
+const z = [1, 2, 3, 4];
+
+console.log(z.slice(1));
